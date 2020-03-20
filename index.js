3c93c629c5b92546e726918c1f53a69158a57fd8
sharmainegaw <sharmaine_gaw@dlsu.edu.ph>
2020-03-20 11:07:30 +0800

    fixed errors

--- index.js
+++ index.js
@@ -1,147 +1,236 @@
 const express = require('express');
-const hbs = require('hbs');
-const port = 9090
+const path = require('path');
+const exphbs = require('express-handlebars');
+const handlebars = require('handlebars');
 
+const port = 9090
 const app = express();
 
-const loggedIn = false;
-const adminloggedin = false;
+app.engine('hbs', exphbs({
+    extname: 'hbs',
+    defaultView: 'main',
+    layoutsDir: path.join(__dirname, '/views/layouts'),
+    partialsDir: path.join(__dirname, '/views/partials')
+}));
 
 app.set('view engine', 'hbs');
 
-app.use(express.static('views'));
+const loggedIn = false;
+//const adminUser = false;
 
-app.get('/', function(req, res) {
-    if(!loggedIn)
-        res.sendFile(__dirname + "home.html");
-    else
-        res.sendFile(__dirname + "homepage.html");
-});
+/* HOME AND REGISTER */
 
-app.get('/home', function(req, res) {
-    if(!loggedIn)
-        res.sendFile(__dirname + "home.html");
-    else
-        res.sendFile(__dirname + "homepage.html");
-});
-
-app.get('/register', function(req, res) {
-    res.sendFile(__dirname + "register.html");
-});
-
-app.get('/:username', function(req, res) {
-    if(!loggedIn)
-        res.sendFile(__dirname + "home.html");
-    else if (adminloggedin)
-        res.sendFile(__dirname + "adminprofile.hbs");
-    else
-        res.sendFile(__dirname + "profile.hbs");
-});
-
-app.get('/:postId', function(req, res) {
-    if(!loggedIn)
-        res.sendFile(__dirname + "home.html");
-    else if (adminloggedin)
-        res.sendFile(__dirname + "adminpost.hbs");
-    else
-        res.sendFile(__dirname + "post.hbs");
-});
-
-app.get('/ffs', function(req, res) {
-    if(!loggedIn)
-        res.sendFile(__dirname + "home.html");
-    else
-        res.sendFile(__dirname + "ffs.hbs");
-});
-
-app.get('/admin', function(req, res) {
-    if(!loggedIn)
-        res.sendFile(__dirname + "home.html");
-    else
-        res.sendFile(__dirname + "adminposts.hbs");
-});
-
-app.get('/adminusers', function(req, res) {
-    if(!loggedIn)
-        res.sendFile(__dirname + "home.html");
-    else
-        res.sendFile(__dirname + "adminusers.hbs");
-});
-/*
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + "home.html");
-});
-
-app.get('/home', function(req, res) {
-    res.sendFile(__dirname + "home.html");
-});
-
-hbs.registerHelper('cap', function(text) {
-    return text.toUpperCase();
-});
-
-hbs.registerHelper('bold', function(text) {
-    var x = '<b>' + text + '</b>';
-
-    return x;
-});
-
-hbs.registerHelper('isFriend', function(friend) {
-    var value;
-    if(friend)
-        value = 'Unfriend';
-    else value = 'Add Friend';
-
-    return value;
-})
-
-hbs.registerHelper("link", function(text, url) {
-    var x = '<a href=' + url + '>' + text + '</a>';
-
-    return x;
-});
-
-hbs.registerPartials(__dirname + '/views/partials');
-
-app.get('/profile', function(req, res) {
-    
-//    res.render('profile', {
-//        title: "Mr.",
-//        fn: "Ned",
-//        ln: "Stark"
-//    });
-
-    res.render("profile", {
-        status: false
+app.get('/', function(req, res){
+    res.render('home', {
+        title: "bids++"
     });
-
 });
 
-app.get('/friends', function(req, res) {
-    res.render("friends", {
-        friends: [
-            {fn: "Sharmaine", ln: "Gaw"},
-            {fn: "Robi", ln: "Banogon"}
+app.post('/', function(req, res){
+    res.render('home', {
+        title: "bids++"
+    });
+});
+
+app.get('/registration', function(req, res){
+    res.render('registration', {
+        title: "register"
+    });
+});
+
+
+
+/* USER VIEW - HOME AND POSTS */
+
+app.post('/home', function(req, res){
+    res.render('homepage', {
+        useravatar: "img/default.png",
+        title: "home",
+        post: [
+            {
+                title: 'off-shoulder cropped top',
+                tag: [
+                    {tagname: "women's"}, {tagname: "top"} 
+                ],
+                postername: "sharmainegaw",
+                posteravatar: "img/default.png",
+                stealprice: 200,
+                currentprice: 150,
+                itemimg: "img/default.png",
+                date: "march 20",
+                time: "idk"
+            },
+            {
+                title: 'weee gumagana',
+                tag: [
+                    {tagname: "misc"}
+                ],
+                postername: "christinedtc",
+                posteravatar: "img/default.png",
+                stealprice: 300,
+                currentprice: 180,
+                itemimg: "img/default.png",
+                date: "march 20",
+                time: "idk"
+            }
         ]
     });
-    
 });
 
 app.get('/home', function(req, res){
-    //res.render("home", {title: "Homepage"});
+    res.render('homepage', {
+        useravatar: "img/default.png",
+        title: "home",
+        post: [
+            {
+                title: 'off-shoulder cropped top',
+                tag: [
+                    {tagname: "women's"}, {tagname: "top"} 
+                ],
+                postername: "sharmainegaw",
+                posteravatar: "img/default.png",
+                stealprice: 200,
+                currentprice: 150,
+                itemimg: "img/default.png",
+                date: "march 20",
+                time: "idk"
+            },
+            {
+                title: 'weee gumagana',
+                tag: [
+                    {tagname: "misc"}
+                ],
+                postername: "christinedtc",
+                posteravatar: "img/default.png",
+                stealprice: 300,
+                currentprice: 180,
+                itemimg: "img/default.png",
+                date: "march 20",
+                time: "idk"
+            }
+        ]
+    });
+});
 
-    // res.render("home", {
-    //     label : "Google",
-    //     url : "https://www.google.com"
-    // });
-
-    res.render('home', {
-        fn: 'Ned',
-        ln: 'Stark'
+app.get('/ffs', function(req, res){
+    res.render('ffpage', {
+        useravatar: "img/default.png",
+        title: 'ffs',
+        post: [
+            {
+                title: "weee gumagana",
+                tag: [
+                    {tagname: "misc"}
+                ],
+                postername: "christinedtc",
+                posteravatar: "img/default.png",
+                stealprice: 300,
+                currentprice: 180,
+                itemimg: "img/default.png",
+                date: "march 20",
+                time: "idk"
+            }
+        ]
     });
 });
 
 
+
+/* (USER) VIEW POST */
+
+//TODO
+
+
+
+/* (USER) PROFILE */
+
+app.get('/profile', function(req, res){
+    res.render('profile', {
+        useravatar: "img/default.png",
+        title: "profile",
+        profileusername: "nemo",
+        dp: "img/default.png",
+        checkedstars: [
+            {},{},{}
+        ],
+        bio: "woof woof",
+        contactdetails: "911",
+        post: [
+            {
+                title: "weee gumagana",
+                tag: [
+                    {tagname: "misc"}
+                ],
+                postername: "christinedtc",
+                posteravatar: "img/default.png",
+                stealprice: 300,
+                currentprice: 180,
+                itemimg: "img/default.png",
+                date: "march 20",
+                time: "idk"
+            }
+        ]
+    });
+});
+
+
+
+/* (USER) EDIT PROFILE */
+
+//TODO
+
+
+
+/* (USER) REVIEWS */
+
+//TODO
+
+
+
+/* (USER) CREATE POST */
+
+//TODO
+
+
+
+/* (USER) EDIT POST */
+
+//TODO
+
+
+
+/* (ADMIN) POSTS*/
+
+//TODO
+
+
+
+/* (ADMIN) PROFILE*/
+
+//TODO
+
+
+
+/* (ADMIN) REVIEWS*/
+
+//TODO
+
+
+
+/* (ADMIN) USER*/
+
+//TODO
+
+
+
+/* (ADMIN) VIEW POST*/
+
+//TODO
+
+
+
+app.use(express.static('public'));
+
 app.listen(port, function() {
     console.log('Listening at port ' + port); 
-}); */
+});
