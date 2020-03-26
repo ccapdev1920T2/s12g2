dfa3fae332230253375794eebc9ef2df0faf9225
hawkeye1006 <58631507+hawkeye1006@users.noreply.github.com>
2020-03-26 21:21:27 +0800

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -10,10 +10,11 @@ const app = express();
 // when a client sends an HTTP GET request for `/favicon.ico`
 app.get('/favicon.ico', controller.getFavicon);
 
+// when a client sends an HTTP GET request for `/editprofile`
 app.get('/editprofile', controller.editProfile);
 
 // execute function getFavicon() as defined in object controller in `../controllers/controller.js`
-// when a client sends an HTTP GET request for `/home`
+// when a client sends an HTTP GET request for `/:username`
 // hmm feeling ko dapat '/user/:username' nalang to? HAHAHAH IDK NA
 app.get('/:username', controller.getProfile);
 
