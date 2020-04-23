f1e9c3ef6534b80c74c1f2611169206e9d062a62
hawkeye1006 <58631507+hawkeye1006@users.noreply.github.com>
2020-04-23 18:59:30 +0800

    added for heroku

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const handlebars = require('handlebars');
 
 const routes = require('./routes/routes.js');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 
 const admin = false;
